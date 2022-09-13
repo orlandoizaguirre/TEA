663a3c3b66ed5ef0378a87995e9e3c41fb01db24
orlandoizaguirre <112192110+orlandoizaguirre@users.noreply.github.com>
2022-09-12 23:09:16 -0600

    Add files via upload

---
+++ lab02/lab02cadena.py
@@ -0,0 +1,6 @@
+#Tendencias e Inovacion en Tecnologias Agricolas (TEA)
+x = "Hola, mundo"
+#display x:
+print (x)
+#display the data type of x:
+print (type(x))
---
+++ lab02/lab02numerico.py
@@ -0,0 +1,14 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+from tkinter import X
+
+
+X= 20
+#display x: 
+print(X)
+#display the data type of x:
+print(type(X))
+y= 20.5
+#display y:
+print(y)
+#display the data type of y:
+print(type(y))
