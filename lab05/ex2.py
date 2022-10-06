e8efe082c4960d29923ac1e3d61ee3d03b767493
orlandoizaguirre <112192110+orlandoizaguirre@users.noreply.github.com>
2022-10-05 19:04:40 -0600

    Add files via upload

---
+++ lab05/ex2.py
@@ -0,0 +1,8 @@
+try:
+    entrada = input("Ingrese nombre del archivo: ")
+    archivo = open(entrada)
+    for linea in archivo:
+        print(linea.upper()) 
+except:
+    print("Error, no existe el archivo")
+    
